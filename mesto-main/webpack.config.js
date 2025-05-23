cf9b029ad42b64fd004172d3972512ad1f795302
m1staken <67109667+m1staken@users.noreply.github.com>
2025-05-23 18:06:22 +0300

    Update webpack.config.js

--- mesto-main/webpack.config.js
+++ mesto-main/webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    main: './src/scripts/index.js',
+    main: './mesto-main/src/scripts/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
